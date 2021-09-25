02e83ba6fd49673e2720eaf65fb67c0598bc5f41
Cosdaman <Cosdaguy@gmail.com>
2021-09-25 11:52:05 -0700

    added boolean check

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var loopCheck = 0;
+
+//validation for yes and no
+function booleanCheck(x) {
+  if (x.toLowerCase() == "yes" || x.toLowerCase() == "y") {
+    x = true;
+  } else if (x.toLowerCase() == "no" || x.toLowerCase() == "n") {
+    x = false;
+  } else {
+    alert("Please type either 'Yes' or 'No'. This is not case sensitive. ");
+    return loopCheck = 1;
+  }
+  return x;
+}
 
 //Generate password function
-function generatePassword(){
+function generatePassword() {
 
   //password length entry and validation
   var pwLength = prompt("Please enter the length of the password.", "Enter a number between 8 and 128.");
-  if(isNaN(pwLength) || pwLength===null){
-    return alert("Password length must be a number.")
-  }else if(pwLength<8){
+  if (isNaN(pwLength) || pwLength === null) {
+    return alert("Password length must be a number.");
+  } else if (pwLength < 8) {
     return alert("Password length cannot be less than 8.");
-  }else if(pwLength>128){
+  } else if (pwLength > 128) {
     return alert("Password length cannot be greater than 128.");
   }
+  console.log(pwLength);
 
   var pwLowCase = prompt("Do you want to include lowercase characters?", "Yes or No");
+  booleanCheck(pwLowCase);
   var pwUpCase = prompt("Do you want to include uppercase characters?", "Yes or No");
   var pwNum = prompt("Do you want to include numeric characters?", "Yes or No");
   var pwSpChar = prompt("Do you want to include special characters?", "Yes or No");
-  
+
 }
 
 // Write password to the #password input
